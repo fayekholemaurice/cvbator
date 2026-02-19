d0dfccf3bbaeada8cadb7a13796284c30a44cb01
fayekholemaurice <fayekholemaurice@gmail.com>
2026-02-19 11:39:54 +0000

    Add files via upload

---
+++ cvbator.py
@@ -0,0 +1,53 @@
+import streamlit as st
+
+st.title("Mon CV Professionnel")
+
+st.sidebar.header("Mes Informations Personelles")
+st.sidebar.write("**Nom :**Ndeye Bator DIEYE")
+st.sidebar.write("**Email :*batorndeye@gmail.com*")
+st.sidebar.write("**Téléphone:**768676286")
+
+st.markdown("....")
+
+#Profil
+st.header("Profil")
+st.write("Technicienne Supérieure en Géomatique") 
+
+st.markdown("...")
+
+#Formation
+st.header("Formation")
+st.write("Géomatique")
+
+st.markdown("....")
+
+#Compétence
+st.header("compétences en Géomatique")
+st.write("Analyse de données")
+st.write("Collecte de données géospatiales")
+st.write("Gestion de base de données geospatiales")
+st.write("Cartographie")
+st.write("Maitrise des logiciels de cartographie")
+
+st.header("compétences en Informatique bureautique")
+st.write("Word")
+st.write("Excel")
+st.write("Power point")
+
+st.header("Aptitude Personnel")
+st.write("J'ai un esprit collaboratif, et de travail en équipe")
+st.write("Je m'adapte vite")
+
+st.markdown("...")
+
+#Langue:
+st.header("langues")
+st.write("Anglais moyenne")
+st.write("Français")
+
+st.markdown("...")
+
+#Projet 
+st.header("projets académiques")
+st.write("Je souhiate mettre en place un cabinet de consultance en géomatique")
+
